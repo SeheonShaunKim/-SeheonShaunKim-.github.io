18bf69f802251cb9343da884e8a82f80d2762a96
Shaun Kim <rlatpgjs00@snu.ac.kr>
2024-05-16 12:24:09 +0900

    ok

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715089685',resources: [
+    cacheName: 'chirpy-1715829796',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
